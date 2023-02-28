01a8900e102f8a359d9d719795256e14e64e0395
Tino <tino5280@gmail.com>
2023-02-27 17:20:05 -0700

    Fin lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+// Write your solution 
+
+
+let cats = [ 'Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+function appendCat(name){
+    let moreCats = [...cats, name]
+    return moreCats
+}
+
+function prependCat(name){
+    let moreCats = [name, ...cats]
+    return moreCats
+}
+
+function removeLastCat(){
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
